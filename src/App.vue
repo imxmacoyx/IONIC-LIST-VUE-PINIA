32f91697ff96b06cbb608cef9e6bbd83fee4f73d
Marco Valle <marco_97@live.com.mx>
2024-02-07 13:26:23 -0600

    feat: Se agrega zona segura para notch de iphone.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
   <ion-app>
+    <ion-content :style="{ 'padding-top': 'env(safe-area-inset-top)', 'padding-bottom': 'env(safe-area-inset-bottom)' }">
     <ion-router-outlet />
+    </ion-content>
   </ion-app>
 </template>
 
 <script setup lang="ts">
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 </script>
+
+<style>
+  ion-content {
+    --padding-top: var(--ion-safe-area-top);
+    --padding-bottom: var(--ion-safe-area-bottom);
+  }
+</style>
